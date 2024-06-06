5f95addbc366acffa9a332973f9aa5182e95e252
daniel.kadlec <daniel.kadlec@VT3-01>
2024-06-06 11:50:29 +0200

    Přidejte soubory projektu.

---
+++ Videopřehrávač/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Videopřehrávač
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool isPlaying = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void play_Click(object sender, RoutedEventArgs e)
+        {
+            if (isPlaying)
+            {
+                media.Pause();
+                play.Content = "Play";
+                isPlaying = false;
+            }
+            else
+            {
+                media.Play();
+                play.Content = "Stop";
+                isPlaying = true;
+            }
+        }
+
+        private void media_MediaFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            Debug.WriteLine(e.ErrorException.Message);
+        }
+    }
+}
